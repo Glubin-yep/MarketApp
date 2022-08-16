f0de79b9fdf0828daf8668826e8584e645016c44
Dima <dima543439@gmail.com>
2022-08-16 22:33:43 +0300

    Add Money,Nickname,Avatar

---
+++ MarketBot/HttpGetInfo.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketBot
+{
+    class HttpGetInfo
+    {
+        public static string GetMoney()
+        {
+            string responseData = string.Empty;
+            const string actionUrl = "https://market.csgo.com/api/v2/get-money?key=5Gpq3KhWO0u4t3L60mYY9VLzsjuv389";
+
+            HttpWebRequest request = WebRequest.Create(actionUrl) as HttpWebRequest;
+            request.Method = "GET";
+            request.ContentType = "application/json";
+
+            using (HttpClient client = new HttpClient())
+
+            {
+
+                try
+                {
+                    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                    {
+                        using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                        {
+                            responseData = reader.ReadToEnd();
+                            reader.Close();
+                        }
+
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                
+                return responseData;
+            }
+
+        }
+        public static string GetAvatar()
+        {
+            string responseData = string.Empty;
+            const string actionUrl = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=00F44B78D419316F764EAD327522119D&steamids=76561198829066528";
+
+            HttpWebRequest request = WebRequest.Create(actionUrl) as HttpWebRequest;
+            request.Method = "GET";
+            request.ContentType = "application/json";
+
+            using (HttpClient client = new HttpClient())
+
+            {
+
+                try
+                {
+                    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                    {
+                        using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                        {
+                            responseData = reader.ReadToEnd();
+                            reader.Close();
+                        }
+
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+            return responseData;
+        }
+    }
+    
+    
+}
--- MarketBot/MainWindow.xaml.cs
+++ MarketBot/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,14 +21,33 @@ namespace MarketBot
 {
     public partial class MainWindow : Window
     {
+       
+        
+
         public MainWindow()
         {
             InitializeComponent();
+
+            var balans = JsonConvert.DeserializeObject<Balans>(HttpGetInfo.GetMoney());
+            Money.Content = balans.money.ToString();
+            var user = JsonConvert.DeserializeObject<User>(HttpGetInfo.GetAvatar());
+
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = new Uri($"{user.response.players.First().avatarfull}"); ;
+            bitmapImage.EndInit();
+            Photo.Source = bitmapImage;
+
+            Nickname.Content = user.response.players.First().personaname;
+
         }
 
-        private void Test_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("");
+          private void Test_Click(object sender, RoutedEventArgs e)
+
+          {
+            
         }
+
+        
     }
 }
