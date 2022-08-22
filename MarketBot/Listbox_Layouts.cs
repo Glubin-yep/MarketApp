76723c28f17b4735c9d7e9b445d2b85f825f6b02
Dima <dima543439@gmail.com>
2022-08-22 13:30:35 +0300

    Aded custom ListBox and market history

---
+++ MarketBot/Listbox_Layouts.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace MarketBot
+{
+    internal class LayoutListbox : ListBox
+    {
+        public Layout layout
+        {
+            get { return (Layout)GetValue(layoutProperty); }
+            set { SetValue(layoutProperty, value); }
+        }
+
+        public static readonly DependencyProperty layoutProperty = DependencyProperty.Register("layout", typeof(Layout), typeof(LayoutListbox), new PropertyMetadata(Layout.Tile));
+
+    }
+
+    public enum Layout
+    {
+        Tile
+    }
+}
