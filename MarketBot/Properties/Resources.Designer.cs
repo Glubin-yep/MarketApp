617d193276dd888a34f07ad0aae1d52eecd54038
Dima <dima543439@gmail.com>
2022-09-05 12:41:23 +0300

    Rename

--- MarketBot/Properties/Resources.Designer.cs
+++ MarketBot/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MarketBot.Properties {
+namespace MarketApp.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MarketBot.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MarketBot.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MarketApp.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
