33cb319b499a401924ff0a406f9057e6003175c0
Dima <dima543439@gmail.com>
2022-11-27 03:12:20 +0200

    Add auto load

---
+++ MarketBot/Settings/Utills.cs
@@ -0,0 +1,30 @@
+﻿namespace MarketApp.Settings
+{
+    class Utills
+    {
+        public static void AddToAutoLoad()
+        {
+            try
+            {
+                string BaseDir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+                Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+                key.SetValue("MarketApp", System.IO.Path.Combine(BaseDir, "MarketApp.exe"));
+            }
+            catch { }
+        }
+        public static void RemoveFromAutoLoad()
+        {
+            try
+            {
+                string BaseDir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+                Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+                key.DeleteValue("MarketApp", false);
+            }
+            catch { }
+        }
+    }
+}
