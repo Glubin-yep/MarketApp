86f1ddcf40627acd669b60cedfbb43213e4b76d6
Dmytro Chyr <dima543439@gmail.com>
2023-05-14 01:34:10 +0300

    Fix regedit path save

--- MarketCore/Utills/Regedit.cs
+++ MarketCore/Utills/Regedit.cs
@@ -6,14 +6,17 @@ namespace MarketCore.Utills
     {
         public static void AddToAutoLoad()
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            try
             {
-                string BaseDir = AppDomain.CurrentDomain.BaseDirectory;
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && Environment.ProcessPath != null)
+                {
+                    string exePath = Environment.ProcessPath;
 
-                Microsoft.Win32.RegistryKey? key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-
-                key?.SetValue("MarketApp", Path.Combine(BaseDir, "MarketApp.exe"));
+                    Microsoft.Win32.RegistryKey? key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                    key?.SetValue("MarketApp", exePath);
+                }
             }
+            catch { }
         }
         public static void RemoveFromAutoLoad()
         {
